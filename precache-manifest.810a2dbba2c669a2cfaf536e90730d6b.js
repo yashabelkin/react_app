5d31bda5a6aeb37892c786a477952de741f87a15
yashabelkin <100130494+yashabelkin@users.noreply.github.com>
2022-06-15 09:58:32 +0300

    Add files via upload

---
+++ precache-manifest.810a2dbba2c669a2cfaf536e90730d6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f11cfa6f88c8d46ecdac",
+    "url": "/static/css/main.63985fa0.chunk.css"
+  },
+  {
+    "revision": "f11cfa6f88c8d46ecdac",
+    "url": "/static/js/main.f11cfa6f.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "2ded161d425ce65a7129",
+    "url": "/static/js/2.2ded161d.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "de973c1a1ddd0603fffbd686bf33ce1c",
+    "url": "/index.html"
+  }
+];
